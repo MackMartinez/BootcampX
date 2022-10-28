af596399e4e27e73494fbbd13738df9cb3a8f7dd
MackyClap <martimac2320@gmail.com>
2022-10-28 00:27:48 -0400

    query avg completiong time by student

---
+++ 3_queries_group-by/5_avg_completion_time.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM assignment_submissions;
+-- SELECT * FROM students;
+
+SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration
+  FROM assignment_submissions
+  JOIN students ON student_id = students.id
+  WHERE students.end_date IS NULL
+  GROUP BY student
+  ORDER BY average_assignment_duration DESC;
